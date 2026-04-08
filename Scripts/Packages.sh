72b1c309518694f57f26f6fbbce4eb323c9a8045
Aotuman <yu5200.lee@hotmail.com>
2026-04-08 09:27:41 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -76,7 +76,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "luci-app-store" "linkease/istore" "main"
+#UPDATE_PACKAGE "luci-app-store" "linkease/istore" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
